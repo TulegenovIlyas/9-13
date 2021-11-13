d8aa0659f69b5be0e06a113ea4fa3241a8e71593
TulegenovIlyas <93704928+TulegenovIlyas@users.noreply.github.com>
2021-11-13 13:08:06 +0600

    Add files via upload

---
+++ Example.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="wrapper-content wrapper-content--fixed">
+      <section>
+        <div class="container">
+            <h1>Example page</h1>
+            <!-- <input type="text" v-model="message">
+            <button class="btn btnPrimary" @click=" setMessage ">Change message</button>
+            <br>
+            <br> -->
+        </div>
+      </section>
+    </div>
+</template>
+
+// <script>
+// export default {
+//   data ()
+//   {
+//     return {
+//       message: null,
+//     }
+//   },
+//   computed: {
+//     getMessage () {
+//       return this.$store.getters.getMessage
+//     },
+//     methods: {
+//       setMessage () {
+//         this.store.dispatch('setMessage', this.message)
+//       }
+//     }
+//   }
+// }
+// </script>
---
+++ Hom.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="wrapper-content wrapper-content--fixed">
+      <section>
+        <div class="container">
+            <h1>Home page</h1>
+            <!-- {{ message }} -->
+        </div>
+      </section>
+    </div>
+</template>
+
+// <script>
+// export default {
+//   computed: {
+//     message () {
+//       return this.$store.getters.getMessage
+//     }
+//   }
+// }
+// </script>
